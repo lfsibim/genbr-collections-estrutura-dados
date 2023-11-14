3bd789a7ecbcd8fa54743e61a7a21ac3433117ef
Luis Sibim <141850109+lfsibim@users.noreply.github.com>
2023-11-14 13:08:51 -0300

    Adicionado exercicios collections list e set

---
+++ List_Exercicio1.java
@@ -0,0 +1,29 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class List_Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		ArrayList<String> cores = new ArrayList();
+		
+		for(int x=1; x<=5; x++) {
+			System.out.println("\nDigite a "+x+"ª cor: ");
+			String cor = input.nextLine();
+			cores.add(cor);			
+		}
+		
+		System.out.println("\nLista de cores: "+cores);
+		
+		Collections.sort(cores);
+		System.out.println("\nCores ordenadas: "+cores);
+		
+		
+
+	}
+
+}
---
+++ Set_Exercicio1.java
@@ -0,0 +1,31 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Set_Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		Set<Integer> numeros = new HashSet<Integer>();
+		
+		System.out.println("Digite 10 números inteiros não repetidos.");
+		for(int i=1; i<=10; i++) {
+			System.out.println("\n"+i+"º número: ");
+			int num = input.nextInt();
+			numeros.add(num);
+		}
+		
+		Iterator<Integer> iNumeros = numeros.iterator();
+		
+		System.out.println("\nListar dados: \n");
+		while(iNumeros.hasNext()) {
+			System.out.println(iNumeros.next());
+		}
+
+	}
+
+}
